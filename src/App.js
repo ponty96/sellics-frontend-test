d8d6c76bde07aa5007d79e6316d1c7cc90bf0b3c
Ayomide Aregbede <ayo.aregbede@gmail.com>
2019-02-27 19:57:50 +0100

    ui updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppWrapper, Button, FilterPaneWrapper, ReviewsWrapper, RatingsFilter, Input, Select, CheckboxWrapper } from './app.styles';
+import ReviewItem from './components/ReviewItem';
 
 class App extends Component {
   render() {
+    const reviews = [1, 2, 3].map((item, i) => {
+      return (
+        <ReviewItem />
+      );
+    })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppWrapper>
+        <div className="col-md-6 col-md-offset-1">
+          <FilterPaneWrapper className="row">
+            <div className="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+              <div className="row">
+                <div className="col-xs-12 col-md-12">
+                  <Input type="text" placeholder="Search" />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-xs-6 col-md-6">
+                  <Select>
+                    <option>Group by</option>
+                  </Select>
+                </div>
+                <div className="col-xs-6 col-md-6">
+                  <Select>
+                    <option>Order by</option>
+                  </Select>
+                </div>
+              </div>
+              <RatingsFilter className="row">
+                <div className="col-xs-12 col-md-12">
+                  <span className="">FILTER BY</span>
+                  <CheckboxWrapper>
+                    <input type="checkbox" /> <span>1<i className="mdi mdi-star" /></span> 
+                    <input type="checkbox" /> <span>2<i className="mdi mdi-star" /></span> 
+                    <input type="checkbox" /> <span>3<i className="mdi mdi-star" /></span> 
+                    <input type="checkbox" /> <span>4<i className="mdi mdi-star" /></span> 
+                    <input type="checkbox" /> <span>5<i className="mdi mdi-star" /></span> 
+                  </CheckboxWrapper>
+                </div>
+              </RatingsFilter>
+            </div>
+          </FilterPaneWrapper>
+          <ReviewsWrapper className="row">
+            <div className="col-xs-12 col-md-12">
+              <div className="row end-xs">
+                <div className="col-xs-12 col-md-12">
+                  <Button>REFRESH</Button>
+                </div>
+              </div>
+              {reviews}
+            </div>
+          </ReviewsWrapper>
+        </div>
+      </AppWrapper>
     );
   }
 }
---
+++ src/colors.js
@@ -0,0 +1,7 @@
+export default {
+    blue: '#2590C9',
+    white: '#fff',
+    ashBlue: '#86A5B5',
+    grey: '#a3a3a3',
+    black: '#000'
+}
